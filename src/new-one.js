779ec8764d36e13e93bfd303a17c3ef2413eb6fe
ManSuna <gnuruglinus@gmail.com>
2025-07-10 23:12:42 -0400

    Changed something

--- src/new-one.js
+++ src/new-one.js
@@ -1,27 +1,3 @@
-return (
-  <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="de">
-    <Container sx={{ mt: 4 }}>
-      <Box display="flex" justifyContent="space-between" mb={2}>
-        <h2>Interest Rate Grid</h2>
-        <Button variant="contained" onClick={() => setShowForm(true)}>
-          Add Interest Rate
-        </Button>
-      </Box>
-
-      {/* 🔽 Show form on top */}
-      {showForm && (
-        <Box mb={2}>
-          <AddForm
-            onAdd={(data) => handleAdd({ ...data, id: uuidv4() })}
-            onCancel={() => setShowForm(false)}
-          />
-        </Box>
-      )}
-
-      {/* 🔽 Grid below form */}
-      <InterestGrid rows={rows} onDelete={handleDelete} />
-
-      <ConfirmDialog {...confirm} onClose={() => setConfirm({ open: false })} />
-    </Container>
-  </LocalizationProvider>
-);
+<Button variant="contained" onClick={() => setShowForm((prev) => !prev)}>
+  {showForm ? 'Cancel' : 'Add Interest Rate'}
+</Button>
