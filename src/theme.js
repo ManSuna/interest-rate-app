335e4ee99d2ae72ad58541af7cbc6d5f19c8c7ee
ManSuna <gnuruglinus@gmail.com>
2025-08-13 14:35:51 -0400

    Changed something

--- src/theme.js
+++ src/theme.js
@@ -1,28 +1,17 @@
-import { Table, TableBody, TableCell, TableHead, TableRow, Typography } from "@mui/material";
-
-<Typography variant="caption" color="text.secondary" sx={{ fontWeight: 500 }}>
-  Interest Rates
-</Typography>
-
-{(data.interestRates ?? []).length > 0 ? (
-  <Table size="small" sx={{ mt: 1, width: "100%" }}>
-    <TableHead>
-      <TableRow>
-        <TableCell sx={{ fontWeight: 600 }}>Rate (%)</TableCell>
-        <TableCell sx={{ fontWeight: 600 }}>Start Date</TableCell>
-      </TableRow>
-    </TableHead>
-    <TableBody>
-      {data.interestRates.map((r, idx) => (
+<Table size="small" sx={{ mt: 1, width: "100%" }}>
+  <TableHead>
+    <TableRow>
+      <TableCell align="right" sx={{ fontWeight: 600 }}>Rate (%)</TableCell>
+      <TableCell sx={{ fontWeight: 600 }}>Start Date</TableCell>
+    </TableRow>
+  </TableHead>
+  <TableBody>
+    {[...data.interestRates].sort((a, b) => new Date(b.startDate) - new Date(a.startDate))
+      .map((r, idx) => (
         <TableRow key={idx}>
-          <TableCell>{Number(r.rate).toFixed(2)}</TableCell>
+          <TableCell align="right">{Number(r.rate).toFixed(2)}</TableCell>
           <TableCell>{fmt(r.startDate)}</TableCell>
         </TableRow>
-      ))}
-    </TableBody>
-  </Table>
-) : (
-  <Typography variant="body2" color="text.disabled">
-    No interest rates available
-  </Typography>
-)}
+    ))}
+  </TableBody>
+</Table>
