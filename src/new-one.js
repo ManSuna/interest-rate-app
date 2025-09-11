ba2e51053899e693ea6034b19ebc879c927de687
ManSuna <gnuruglinus@gmail.com>
2025-09-11 11:00:21 -0400

    Update new-one.js

--- src/new-one.js
+++ src/new-one.js
@@ -3,3 +3,10 @@
     ? Number(r.rate).toFixed(2)   // pad with .00
     : r.rate}                     // keep as-is
 </TableCell>
+
+
+<strong>Rate(%):</strong>{" "}
+{Number.isInteger(Number(selectedRow?.rate))
+  ? Number(selectedRow?.rate).toFixed(2)
+  : selectedRow?.rate}
+<br/>
