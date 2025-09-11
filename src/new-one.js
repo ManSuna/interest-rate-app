969eafd1b1d2aa0283acbfb3bc1bc218c1646320
ManSuna <gnuruglinus@gmail.com>
2025-09-11 11:34:30 -0400

    Update new-one.js

--- src/new-one.js
+++ src/new-one.js
@@ -1,12 +1,43 @@
-<TableCell align="right">
-  {Number.isInteger(r.rate)
-    ? Number(r.rate).toFixed(2)   // pad with .00
-    : r.rate}                     // keep as-is
-</TableCell>
+// src/utils/statusUtils.js
+import { Chip, CircularProgress } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import ErrorIcon from "@mui/icons-material/Error";
+import HourglassEmptyIcon from "@mui/icons-material/HourglassEmpty";
 
-
-<strong>Rate(%):</strong>{" "}
-{Number.isInteger(Number(selectedRow?.rate))
-  ? Number(selectedRow?.rate).toFixed(2)
-  : selectedRow?.rate}
-<br/>
+export const renderStatusChip = (resultCode, options = {}) => {
+  switch (resultCode) {
+    case -1:
+      return (
+        <Chip
+          size="small"
+          label="In Progress"
+          color="warning"
+          icon={
+            options.spinner ? (
+              <CircularProgress size={14} />
+            ) : (
+              <HourglassEmptyIcon />
+            )
+          }
+        />
+      );
+    case 0:
+      return (
+        <Chip
+          size="small"
+          label="Success"
+          color="success"
+          icon={<CheckCircleIcon />}
+        />
+      );
+    default:
+      return (
+        <Chip
+          size="small"
+          label="Failed"
+          color="error"
+          icon={<ErrorIcon />}
+        />
+      );
+  }
+};
